53a5f2856cc25e8a691dd575b52fe83b8f00cb13
letItCurl <rolandlopez.developer@gmail.com>
2021-04-14 11:43:49 +0700

    bom

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
   has_one_attached :picture
   has_rich_text :description
